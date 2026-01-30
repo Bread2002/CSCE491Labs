4b1b68c13e0813ebbf3900a4bcd0579ea4b4c129
Bread2002 <mcryeserver@gmail.com>
2026-01-30 08:03:14 -0500

    Final commit for lab 1 part 1

--- Lab1/code/main.py
+++ Lab1/code/main.py
@@ -6,6 +6,22 @@
 import sys
 import os
 
+
+# Helper function that prints out RD/WR
+def RDWR(index):
+    if mosiList[index + 6] == "0":
+        return "RD"
+    elif mosiList[index + 6] == "1":
+        return "WR"
+    else:
+        return "ERROR"
+    
+    
+# Helper function that converts binary to hex
+def Hex(binary):
+    return f"{int(binary, 2):02x}"
+
+
 ###############################################################################
 
 # This block is setup code that loads the utility library for this assignment.
@@ -33,146 +49,52 @@ w.loadText(sys.stdin.read())
 # Display some information about it. We can use sys.stderr.write() to send a
 # log message to standard error without it interfering with the data we
 # send to standard out for grade.sh to interpret.
-
-
-sys.stderr.write("read a waves file with {} signals and {} samples\n".format(len(w.signals()), w.samples()))
-sys.stderr.write("input has these signals:\n")
-##for s in w.signals():
-    ##sys.stderr.write("\t* {} ({} bits)\n".format(s, w.sizes[s])) 
-   
-   
-"""
-def LongString(column):
-    result = ""
-    for i in range(w.samples()):
-        edge_time = 0 if i == 0 else 1000.0 + (i-1) * 50
-        
-        # This adds the quotes needed for the tool's internal lookup
-        columnName = f'"{column}"' 
-        val = w.signalAt(columnName, edge_time)
-        
-        result += str(val)
-        sys.stderr.write(str(val))
-    return result
-"""    
-
+sys.stderr.write("Read a waves file with {} signals and {} samples...\n".format(len(w.signals()), w.samples()))
+sys.stderr.write("Input has these signals:\n")
+for s in w.signals():
+    sys.stderr.write("\t* {} ({} bits)\n".format(s, w.sizes[s])) 
 
 mosiList = ""
 misoList = ""
+prevSclk = 0  # Track previous clock state to detect edges
+
+# Extract MOSI and MISO data based on SCLK edges
 for i in range(w.samples()):
-    if i ==0:
-        edge_time = 0
-    else:
-        edge_time = 1000.0 + (i-1) * 50
-
-
-    mosiVal = w.signalAt("mosi", edge_time)
-    mosiList = mosiList + str(mosiVal)
+    # Access signal values directly from the data structure
+    # w.data[i] is a tuple: (timestamp, signals_dict)
+    sclkVal = w.data[i][1]["sclk"]
+    mosiVal = w.data[i][1]["mosi"]
+    misoVal = w.data[i][1]["miso"]
+    ssVal = w.data[i][1]["ss"]
+    cpolVal = w.data[i][1]["cpol"]
+    cphaVal = w.data[i][1]["cpha"]
     
-    misoVal = w.signalAt("miso", edge_time)
-    misoList = misoList + str(misoVal)
+    # Only capture on rising edge of sclk (0->1) when ss is active (ss==0)
+    # This is for CPOL=0, CPHA=0 mode
+    if cpolVal == 0 and cphaVal == 0 and ssVal == 0 and sclkVal == 1 and prevSclk == 0:
+        mosiList = mosiList + str(mosiVal)
+        misoList = misoList + str(misoVal)
     
-    ##sys.stderr.write("{}".format(mosiVal))
-    ##sys.stderr.write("{}".format(misoVal))
-sys.stderr.write("\n\n{}".format(mosiList))
-#sys.stderr.write("\n\n{}".format(misoList))
+    prevSclk = sclkVal
 
-## Binary to Hex Converter
-def Hex(binary):
-    return f"{int(binary, 2):02x}"
+# sys.stderr.write("\nMOSI: {}\n".format(mosiList))
+# sys.stderr.write("\nMISO: {}\n".format(misoList))
     
-## Removes the leftover bits
+# Remove any leftover bits
 limit = (len(mosiList) // 16) * 16
 
-## Function to print out Read/Write
-def RDWR(index):
-    if mosiList[index + 6] == "0":
-        return "RD"
-    elif mosiList[index+6] == "1":
-        return "WR"
-    else:
-        return "ERROR"
-
-## Main Loop
+# Main Loop
 for i in range(0, limit, 16):
     address = mosiList[i : i + 6]
-    sys.stderr.write("\n\n{}".format(address))
+    sys.stderr.write("\nAddress #{}: {}".format(i // 16 + 1, address))
     
-    SIST = mosiList[i + 7]
-    sys.stderr.write("\n\n{}".format(SIST))
+    SIST = mosiList[i + 6]
+    sys.stderr.write("\nSIST: {}".format(SIST))
     
     if RDWR(i) == "RD":
-        data = mosiList[i + 7 : i + 16]
+        data = misoList[i + 8 : i + 16]
     elif RDWR(i) == "WR":
-        data = misoList[i + 7 : i + 16]
+        data = mosiList[i + 8 : i + 16]
     
+    sys.stderr.write("\nExchange #{}: {}\n".format(i // 16 + 1, data))
     sys.stdout.write("{} {} {}\n".format(RDWR(i), Hex(address), Hex(data)))
-
-
-
-
-
-'''
-binary_str = "10101011"
-# Lowercase without 0x
-print(f"{int(binary_str, 2):x}")  # Output: 'ab'
-# Uppercase without 0x
-print(f"{int(binary_str, 2):X}")  # Output: 'AB'
-'''
-
-
-
-
-
-"""
-def FindEchoTime(sample):
-    for i in range(w.samples()):
-        if i ==0:
-            edge_time = 0
-        else:
-            edge_time = 1000.0 + (i-1) * 50
-    return edge_time
-    
-sys.stderr.write("value at edge:{}\n".format(FindEchoTime(w.samples())))
- """   
-
-
-
-
-
-"""
-for i in range(w.samples()):
-    if i ==0:
-        edge_time = 0
-    else:
-        edge_time = 1000.0 + (i-1) * 50
-    
-for s in range(6):
-    val = w.signalAt("mosi", edge_time)
-    sys.stderr.write(",{}".format(val))
-
-# val = w.signalAt("mosi", 1000.0)
-# sys.stderr.write("value at edge:{}\n".format(val))
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
